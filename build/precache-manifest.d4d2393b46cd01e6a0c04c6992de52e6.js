94a577bdf9f3bdcaf188d31ab763c114a4530044
Olena Iarmusheva <Olena.Iarmusheva@teaminternational.com>
2019-08-14 17:46:53 +0300

    build

---
+++ build/precache-manifest.d4d2393b46cd01e6a0c04c6992de52e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd195dc7474bd4f31005eb716e8e5fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0263c9bb3176a4ccfaf4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "18bea67ba1bc4ee57003",
+    "url": "/static/css/main.7e7b1d3a.chunk.css"
+  },
+  {
+    "revision": "0263c9bb3176a4ccfaf4",
+    "url": "/static/js/2.42ea5358.chunk.js"
+  },
+  {
+    "revision": "18bea67ba1bc4ee57003",
+    "url": "/static/js/main.e9f6f271.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
